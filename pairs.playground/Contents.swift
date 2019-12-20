f2fca0a16e8dfef9118b6fbd120549f20f44f95f
Aleksander <aleksanderaz@gmail.com>
2019-12-20 19:45:17 +0200

    new idea

--- pairs.playground/Contents.swift
+++ pairs.playground/Contents.swift
@@ -1,42 +1,24 @@
 import UIKit
 import Foundation
 
-// Complete the pairs function below.
 func pairs(k: Int, arr: [Int]) -> Int {
     
-    var countPairs: Int = 0
-    let lastIndex = arr.count
-    let penultIndex = lastIndex - 1
-    var j = 0
-    var  itemPlas = 0
-    var  itemMin = 0
+    var countPairs = 0
+    var newArrAdd = arr + arr.map( { $0 + k})
+    let lastIndexAdd = newArrAdd.count - 1
+    
+    if ( lastIndexAdd == 0 ) { return 0 }
+    newArrAdd.sort()
+    
     var i = 0
-    var itemI = 0
-    var itemJ = 0
-    var count = 0
-    
-    
-    while ( i < penultIndex) {
-        itemI = arr[i]
-        itemPlas = itemI + k
-        itemMin = itemI - k
-        count = 0
-        j = i + 1
-        while ( j < lastIndex) {
-            itemJ = arr[j]
-            if (itemJ == itemPlas ) {
-                count += 1
-                if (itemMin < 1) { j = lastIndex }
-            }
-            else if (itemJ == itemMin) {
-                count += 1
-            }
-            if (count > 1) { j = lastIndex }
-            j += 1
+    while (i < lastIndexAdd) {
+        if (newArrAdd[i] ==  newArrAdd[i+1]) {
+            countPairs += 1
+            i += 1
         }
-        countPairs += count
         i += 1
     }
+    
     return countPairs
 }
 
